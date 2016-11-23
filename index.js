4a1c9678e76838431c79c5e6a30430ce2f5c54ba
Tdfrantz <tdfrantz@gmail.com>
2016-11-22 20:10:03 -0500

    Register and Login are both finished and working. Profile is next up.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Express + body-parser
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var uuid = require('node-uuid');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 // Middleware
 // Body Parser
@@ -11,6 +14,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+// Setup sessions
+app.use(session({
+	genid: function(request){
+		return uuid.v4();
+	},
+	resave: false,
+	saveUninitialized: false,
+	secret: 'counter strike global offensive'
+}));
 
 // View engine
 // Views directory, pug files
@@ -23,33 +36,48 @@ mongoose.connect('localhost:27017/diabloUsers');
 // Get the User schema
 var User = require("./models/user.js").User;
 
+// Helper functions
+
+function getUsername(request){
+	var username = '';
+	var session = request.session;
+	if (session.username){
+		username = session.username;
+	}
+	return username;
+}
+
 // Routing
 app.get(['/', '/index'], function(request,response){
-	response.render('index');
+	response.render('index', {username: getUsername(request)});
 });
 
 app.get('/difficultyoverview', function(request,response){
-	response.render('difficultyoverview');
+	response.render('difficultyoverview', {username: getUsername(request)});
 });
 
 app.get('/classes', function(request,response){
-	response.render('classes');
+	response.render('classes', {username: getUsername(request)});
 });
 
 app.get('/legendarygems', function(request,response){
-	response.render('legendarygems');
+	response.render('legendarygems', {username: getUsername(request)});
 });
 
 app.get('/leaderboards', function(request,response){
-	response.render('leaderboards');
+	response.render('leaderboards', {username: getUsername(request)});
 });
 
 app.get('/seasonstats', function(request,response){
-	response.render('seasonstats');
+	response.render('seasonstats', {username: getUsername(request)});
 });
 
 app.get('/register', function(request, response){
 	response.render('register');
+});
+
+app.get('/login', function(request, response){
+	response.render('login');
 });
 
 //processRegistration
@@ -57,19 +85,67 @@ app.post('/processRegistration', function(request, response){
 	var username = request.body.username;
 	var battleTag = request.body.battletag;
 	var battleTagID = request.body.identifier;
-	
-	console.log(username);
-	console.log(battleTag);
-	console.log(battleTagID);
-	
-	
-	
+
 	User.find({username:username}).then(function(results){
-		if (results.length==0){
-			response.render('register', {errorMessage:'Error: Username already taken'})
+		if (results.length!=0){
+			response.render('register', {errorMessage:'Error: Username already taken'});
 		}
 	});
 	
+	//TODO: check if the battletag is unique
+	
+	var session = request.session;
+	session.username = username;
+	session.save();
+	
+	// hash the password
+	var password = request.body.password;
+	var hash = bcrypt.hashSync(password);
+	
+	// save the new user in the database;
+	var email = request.body.email;
+	var newUser = new User({username: username,
+												  battleTag: battleTag,
+												  battleTagID: battleTagID,
+												  email: email,
+												  hashedPassword: hash});
+	
+	newUser.save(function(error) {
+		if (error) {
+			// registration failed
+			response.render('register', {errorMessage:'Something went wrong!'});
+		} else {
+			// registration success
+			response.render('index', {username: username});
+		}
+	});
+});
+
+app.post('/processLogin', function(request,response){
+	var username = request.body.username;
+	var password = request.body.password;
+	
+	User.find({username:username}).then(function(results){
+		// Check if the user exists first
+		if (results.length==0){
+			response.render('login', {errorMessage: 'Error: Username not found!'});
+		} else {
+			// If username is found then check the password
+			if (bcrypt.compareSync(password, results[0].hashedPassword)){
+					var session = request.session;
+					session.username = username;
+					response.render('index', {username: username});
+				}
+				else{
+					response.render('login', {errorMessage: 'Error: Password incorrect!'});
+				}
+		}
+	});	
+});
+
+app.get('/logout', function(request, response){
+	request.session.username='';
+	response.redirect('/index');	
 });
 
 // Run Listener
